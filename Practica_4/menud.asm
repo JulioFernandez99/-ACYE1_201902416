7e55eada37343a9c868d25b1c6f3ecdf7e2cee16
Julio Fernández <juliorod374@gmail.com>
2024-04-07 10:13:32 -0600

    Subiendo practica 4

---
+++ Practica_4/menud.asm
@@ -0,0 +1,188 @@
+INCLUDE MACROS.ASM
+INCLUDE 1vs1\MACROS.ASM
+
+.MODEL small
+.STACK
+.RADIX 16
+.DATA           ;SEGMENTO DE DATOS
+include vars\VARS.ASM
+
+.CODE           ;SEGMENTO DE CODIGO
+inicio:
+    main2 proc
+        mSetDataOnDs
+        ; mCleanScreen
+        ; mPrint datosGenerales
+        ; mPrint newLine
+        ; mPrint mesaje_presionar_enter_continuar
+        ; pauseUntilEnter
+
+        
+        mCleanScreen
+        MOV RECQTY, 0003
+        MOV ACTLINE, 2
+        MOV ACTCOLUMN, 1
+        MOV FONTCOLOR, 0FH
+
+        MOV MARKCOLUMN, 0
+        MOV ACTCOLUMN, 1
+            CALL PPRINTMENUGRAFICO
+            XOR SI, SI
+            MOV BP, BX
+
+            CALL PLOOPMENUGRAFICO
+            
+
+        finish:
+            mTextMode
+            mRetControl
+    main2 endp
+
+    PPRINTMENUGRAFICO PROC
+        CALL PMOVESTOVIDEOMODE
+        mVideoMode
+        mDivLine 09, 29
+        mDivLine 0BBh, 29
+        mPrintTextIntoVideo 00, 0C, mensaje_menu_grafico, 0C, 000F
+        mPrintTextIntoVideo 02, 01, opt1_menu_grafico, 0D, 000F
+        mPrintTextIntoVideo 03, 01, opt2_menu_grafico, 0E, 000F
+        mPrintTextIntoVideo 04, 01, opt3_menu_grafico, 19, 000F
+        mPrintTextIntoVideo 18, 00, opciones_menu_grafico, 27, 000F
+        RET
+    PPRINTMENUGRAFICO ENDP
+
+    PINICIOJUEGO PROC
+        INCLUDE 1vs1\MAIN.ASM
+        RET
+    PINICIOJUEGO ENDP
+
+    PREVIEWNUMBERMARKERDOWN PROC
+        PUSH AX
+        PUSH BX
+        MOV AX, SI
+        MOV BL, 15
+        DIV BL
+        CMP AH, 00
+        JNE finish_review_marker_down
+        MOV MARKLINE, 1
+        MOV ACTLINE, 1
+        ADD MARKCOLUMN, 0C
+        ADD ACTCOLUMN, 0C
+
+        finish_review_marker_down:
+            POP BX
+            POP AX
+            RET
+    PREVIEWNUMBERMARKERDOWN ENDP
+
+    PREVIEWNUMBERMARKERUP PROC
+        PUSH AX
+        PUSH BX
+        MOV AX, SI
+        MOV BL, 15
+        DIV BL
+        CMP AH, 00
+        JNE finish_review_marker_up
+        MOV MARKLINE, 17
+        MOV ACTLINE, 16
+        SUB MARKCOLUMN, 0C
+        SUB ACTCOLUMN, 0C
+
+        finish_review_marker_up:
+            POP BX
+            POP AX
+            RET
+    PREVIEWNUMBERMARKERUP ENDP
+
+    PMARKVALUE PROC
+        mPrintTextIntoVideo MARKLINE, MARKCOLUMN, MARKER, 1, FONTCOLOR
+        RET
+    PMARKVALUE ENDP
+
+    PLOOPMENUGRAFICO  PROC
+        PUSH AX
+        PUSH DX
+
+        MOV AX, SI
+        ADD AX, 2
+        MOV MARKLINE, AL
+        MOV FONTCOLOR, 0A
+        CALL PMARKVALUE
+        MOV SI, 0001
+        MOV ACTCOLUMN, 01
+        MOV ACTLINE, 02
+        mKeysSelectMenuGrafico
+        
+        exit_loop_menu_grafico:
+
+            ; POP DI
+            POP DX
+            POP AX
+            RET
+            
+    PLOOPMENUGRAFICO  ENDP
+
+    PMOVDATATOES PROC
+      PUSH DX
+
+      MOV DX, @DATA
+      MOV ES, DX
+
+      POP DX
+      RET
+    PMOVDATATOES ENDP
+
+    PMOVESTOVIDEOMODE PROC
+      PUSH DX
+
+      MOV DX, 0A000h
+      MOV ES, DX
+
+      POP DX
+      RET
+    PMOVESTOVIDEOMODE ENDP
+
+    CALCPIXELLOC  PROC
+      PUSH BX
+      
+      MOV BX, 140
+      MOV AX, PIXLINE
+      MUL BX
+
+      ADD AX, PIXCOL
+      POP BX
+      
+      RET
+    CALCPIXELLOC ENDP
+
+    DRAWSQUARE  PROC
+      PUSH BX
+      PUSH CX
+
+      MOV BX, AX
+      MOV AX, SQCOLOR
+
+      MOV CX, SQWIDTH
+
+      SQnextLine:
+        SQnextPixel:
+          MOV ES:[BX], AL
+          INC BX
+          LOOP SQnextPixel
+
+        INC AH
+        CMP AH, SQHEIGHT
+        JE SQsalida
+
+        MOV CX, SQWIDTH
+        SUB BX, CX
+        ADD BX, 140
+        JMP SQnextLine
+
+      SQsalida:
+
+      POP CX
+      POP BX
+      RET
+    DRAWSQUARE  ENDP
+END inicio
---
+++ Practica_4/vars/vars.asm
@@ -0,0 +1,242 @@
+;_______________________________________1 vs.1_______________________________________
+
+
+
+;CONTROL DEL TURNO DEL JUGADOR
+turno_jugador db 01
+ganador db 00
+is_celda_invalida db 00
+
+;CORDENADAS DONDE SE COLOCARA LAS OBJETOS
+wallRead db 00
+posXRead dw 00
+posYRead dw 00
+indiceTablero db 00, '$'
+
+; COORDENADAS QUE SE INGRESAN POR TEXTO
+coordenada_x db 00
+coordenada_y db 00
+
+;COORDENADAS CURSOR
+coordenada_c_fila db 00
+coordenada_c_columna db 00
+
+coordenada_limpiar_fila db 00
+
+; MENSAJES
+mensaje_ingresar_coor db 'INGRESAR COORDENADAS: '
+mensaje_celda_invalida db 'La celda ingresada esta ocupada'
+mensaje_ganado_j1 db 'Ha ganado el jugador 1 jugando con X', '$'
+mensaje_ganado_j2 db 'Ha ganado el jugador 2 jugando con O', '$'
+
+;ENTRADA DE TEXTO
+bufferEntrada db 255 dup('$')
+
+;PAREDES TOTITO
+empty_block db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+wall_one    db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+wall_two    db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     01, 01, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+wall_three  db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+wall_four   db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+wall_five   db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+wall_six    db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+wall_ten    db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 01, 01
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+wall_eleven db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     01, 01, 01, 01, 01, 01, 01, 01
+            db     00, 00, 00, 00, 00, 00, 00, 00
+            db     00, 00, 00, 00, 00, 00, 00, 00
+wall_twelve db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 00, 00
+            db     01, 01, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+            db     00, 00, 01, 01, 01, 01, 00, 00
+wall_fourteen   db     00, 00, 01, 01, 01, 01, 00, 00
+                db     00, 00, 01, 01, 01, 01, 00, 00
+                db     00, 00, 01, 01, 01, 01, 01, 01
+                db     00, 00, 01, 01, 01, 01, 01, 01
+                db     00, 00, 01, 01, 01, 01, 01, 01
+                db     00, 00, 01, 01, 01, 01, 01, 01
+                db     00, 00, 00, 00, 00, 00, 00, 00
+                db     00, 00, 00, 00, 00, 00, 00, 00
+wall_fifteen    db     00, 00, 01, 01, 01, 01, 00, 00
+                db     00, 00, 01, 01, 01, 01, 00, 00
+                db     01, 01, 01, 01, 01, 01, 00, 00
+                db     01, 01, 01, 01, 01, 01, 00, 00
+                db     01, 01, 01, 01, 01, 01, 00, 00
+                db     01, 01, 01, 01, 01, 01, 00, 00
+                db     00, 00, 00, 00, 00, 00, 00, 00
+                db     00, 00, 00, 00, 00, 00, 00, 00
+
+
+;FIGURA X
+barrax_uno      db     01, 01, 00, 00, 00, 00, 00, 00
+                db     00, 01, 01, 00, 00, 00, 00, 00
+                db     00, 00, 01, 01, 00, 00, 00, 00
+                db     00, 00, 00, 01, 01, 00, 00, 00
+                db     00, 00, 00, 00, 01, 01, 00, 00
+                db     00, 00, 00, 00, 00, 01, 01, 00
+                db     00, 00, 00, 00, 00, 00, 01, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+
+barrax_dos      db     00, 00, 00, 00, 00, 00, 01, 01
+                db     00, 00, 00, 00, 00, 01, 01, 00
+                db     00, 00, 00, 00, 01, 01, 00, 00
+                db     00, 00, 00, 01, 01, 00, 00, 00
+                db     00, 00, 01, 01, 00, 00, 00, 00
+                db     00, 01, 01, 00, 00, 00, 00, 00
+                db     01, 01, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+
+;FIGURA O
+barrao_uno      db     01, 01, 01, 01, 01, 01, 01, 01
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+
+barrao_dos      db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 00, 00, 00, 00, 00, 00, 00
+                db     01, 01, 01, 01, 01, 01, 01, 01
+
+barrao_tres     db     01, 01, 01, 01, 01, 01, 01, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+
+barrao_cuatro   db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     00, 00, 00, 00, 00, 00, 00, 01
+                db     01, 01, 01, 01, 01, 01, 01, 01
+
+object_map db 09 dup (0)   ;3x3
+
+;SEPARADORES
+separate db '-----------------------------------------------'
+separateTwo db '==============================================='
+
+
+;__________________________________________Menu principal__________________________________________
+
+
+
+; CHAR ESPECIALES
+newLine db 0ah, 0dh, '$'
+
+; DATOS PARA IMPRIMIR EN CONSOLA
+datosGenerales  db 0ah,0dh, 'Universidad de San Carlos de Guatemala', 0ah,0dh, 'Facultad de Ingenieria', 0ah,0dh, 'Escuela de Ciencias y Sistemas', 0ah,0dh, 'Arquitectura de Computadores y Ensambladores 1', 0ah,0dh, 'Seccion A', 0ah,0dh, 'Julio Alfredo Fernandez Rodriguez', 0ah,0dh, '201902416', 0ah,0dh, '$'
+
+
+rotuloAnimacion db 'Bienvenido a las animaciones',0ah,0dh,'$'
+
+;MENSAJES DE CONFIRMACION O ESPERA
+mesaje_presionar_enter_continuar       db   '   Presionar ENTER para continuar ','$'
+; MENU's
+
+
+mensaje_menu_grafico       db 'MENU GRAFICO'
+opciones_menu_grafico       db ' ESC:SALIR            ENTER:SELECCIONAR'
+opt1_menu_grafico db 'Nuevo Juego'
+opt2_menu_grafico db 'Animacion'
+opt3_menu_grafico db 'Informacion'
+opt4_menu_grafico db 'Salir'
+
+; MENSAJES MENUS
+mensaje_inicio_juego db   'Iniciando Juego...','$'
+
+enterOption  db 0ah,0dh, '   Ingresar opcion: ', '$'
+enterValue  db 0ah,0dh, '   Ingresar valor: ', '$'
+
+; PARA EL MENU GRAFICO
+ACTLINE     DB  ?
+ACTCOLUMN   DB  ?
+FONTCOLOR   DW  ?
+MARKLINE    DB  ?
+MARKCOLUMN  DB  ?
+MARKER      DB  ">"
+PIXLINE     DW  ?
+PIXCOL      DW  ?
+; para pintar un cuadrado en el lienzo
+SQCOLOR     DW  ?
+SQHEIGHT    DB  ?
+SQWIDTH     DW  ?
+RECQTY      DW  0000
+SPACE      DB  " "
